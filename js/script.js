244edccc7f72c3c7362f32f4d33e59ef5f86aac6
Verta-master <vertyagina@gmail.com>
2017-09-10 20:54:06 +0600

    Update 2017-09-10T14:53:53.099Z

--- js/script.js
+++ js/script.js
@@ -19,3 +19,16 @@ window.addEventListener("keydown", function (event) {
     }
   }
 });
+
+var navMain = document.querySelector(".menu");
+var navToggle = document.querySelector(".menu__btn");
+
+navToggle.addEventListener("click", function() {
+  if (navMain.classList.contains("menu--closed")) {
+    navMain.classList.remove("menu--closed");
+    navMain.classList.add("menu--opened");
+  } else {
+    navMain.classList.add("menu--closed");
+    navMain.classList.remove("menu--opened");
+  }
+});
